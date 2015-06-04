98bacbffb33285afd959095015c333fd33cd6da8
Macartur Sousa <macartur.sc@gmail.com>
2015-06-04 00:45:03 -0300

    Added mouse-motion and keyboard up,left,right,up,down

--- src/main.py
+++ src/main.py
@@ -2,35 +2,40 @@ from FGAme import *
 from FGAme import init_canvas
 
 
-WIDTH = 800
-HEIGTH = 600
+class Button_Soccer(World):
 
-class Button_Soccer(World):                                                          
-                                                                                 
-    def __init__(self):                                         
+    def __init__(self):
         World.__init__(self, background='blue')  
         self.test = Circle(50, color='red')
+        self.test.pos = (400,300)
         self.add(self.test)
-    
-    @listen("long-press","left")
-    def space(self):
-        x,y = self.test.pos
-        self.test.move(x-0.5,y)
 
-    @listen("long-press","right")
-    def space(self):
-        x,y = self.test.pos
-        self.test.move(x+0.5,y)
+    @listen('long-press','left')
+    def left(self):
+        self.test.move(-5,0)
 
-    @listen("long-press","up")
-    def space(self):
-        x,y = self.test.pos
-        self.test.move(x,y+0.5)
-    @listen("long-press","down")
-    def space(self):
-        x,y = self.test.pos
-        self.test.move(x,y-0.5)
+    @listen('long-press','right')
+    def right(self):
+        self.test.move(5,0)
 
-if __name__ == '__main__':                                                       
-    game = Button_Soccer()                                                           
-    game.run() 
+    @listen('long-press','up')
+    def up(self):
+        self.test.move(0,5)
+
+    @listen('long-press','down')
+    def down(self):
+        self.test.move(0,-5)
+
+    @listen('mouse-motion')
+    def mouse(self,pos):
+        #print(str(pos))
+        pass
+
+    @listen('mouse-click')
+    def mouse_click_l(self,button,pos):
+        print("left")
+        self.test.move(0,-5)
+
+if __name__ == '__main__':
+    game = Button_Soccer()
+    game.run()
